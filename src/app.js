399a369e4d4aa9204fb608901db71c41fb3166ec
PammyLo <pamela@tuminds.com>
2019-10-15 20:34:56 +0100

    added single country display and favourist countries list

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
     data: {
-      countries: []
-
+      countries: [],
+      selectedCountry: null,
+      favouriteCountries: []
     },
     computed: {
       totalPopulation: function () {
@@ -20,7 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch("https://restcountries.eu/rest/v2/all")
           .then(response => response.json())
           .then(data => this.countries = data);
+      },
+      addToFavourites: function(){
+        this.favouriteCountries.push(this.selectedCountry);
       }
+
     }
   });
 });
